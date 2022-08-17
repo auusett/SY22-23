73160147d30d57a1aa52f6ddddaaf14a3aae97c3
auusett <nawel439@learner.dvusd.org>
2022-08-17 13:08:33 -0700

    Wednesday Update

---
+++ VB/bunnydex/bunnydex/Form1.Designer.vb
@@ -0,0 +1,213 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.colorLabel = New System.Windows.Forms.Label()
+        Me.sizeLabel = New System.Windows.Forms.Label()
+        Me.furLabel = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.PictureBoxChinchilla = New System.Windows.Forms.PictureBox()
+        Me.CBPictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.DisplayPictureBox = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBoxChinchilla, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.CBPictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DisplayPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(345, 1)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(141, 29)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Havana"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(521, 123)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 16)
+        Me.Label2.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(454, 46)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(42, 16)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Color:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(455, 62)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(36, 16)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Size:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(455, 78)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(29, 16)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Fur:"
+        '
+        'colorLabel
+        '
+        Me.colorLabel.AutoSize = True
+        Me.colorLabel.Location = New System.Drawing.Point(498, 46)
+        Me.colorLabel.Name = "colorLabel"
+        Me.colorLabel.Size = New System.Drawing.Size(48, 16)
+        Me.colorLabel.TabIndex = 8
+        Me.colorLabel.Text = "Label6"
+        '
+        'sizeLabel
+        '
+        Me.sizeLabel.AutoSize = True
+        Me.sizeLabel.Location = New System.Drawing.Point(498, 62)
+        Me.sizeLabel.Name = "sizeLabel"
+        Me.sizeLabel.Size = New System.Drawing.Size(48, 16)
+        Me.sizeLabel.TabIndex = 9
+        Me.sizeLabel.Text = "Label7"
+        '
+        'furLabel
+        '
+        Me.furLabel.AutoSize = True
+        Me.furLabel.Location = New System.Drawing.Point(498, 78)
+        Me.furLabel.Name = "furLabel"
+        Me.furLabel.Size = New System.Drawing.Size(48, 16)
+        Me.furLabel.TabIndex = 10
+        Me.furLabel.Text = "Label8"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(190, 0)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(149, 30)
+        Me.Button2.TabIndex = 11
+        Me.Button2.Text = "Cinnamon Bunny"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(2, 1)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(172, 29)
+        Me.Button3.TabIndex = 22
+        Me.Button3.Text = "American Chinchilla"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'PictureBoxChinchilla
+        '
+        Me.PictureBoxChinchilla.Image = Global.bunnydex.My.Resources.Resources.images__7_
+        Me.PictureBoxChinchilla.Location = New System.Drawing.Point(2, 30)
+        Me.PictureBoxChinchilla.Name = "PictureBoxChinchilla"
+        Me.PictureBoxChinchilla.Size = New System.Drawing.Size(172, 10)
+        Me.PictureBoxChinchilla.TabIndex = 23
+        Me.PictureBoxChinchilla.TabStop = False
+        '
+        'CBPictureBox1
+        '
+        Me.CBPictureBox1.Image = Global.bunnydex.My.Resources.Resources.images__6_
+        Me.CBPictureBox1.Location = New System.Drawing.Point(190, 30)
+        Me.CBPictureBox1.Name = "CBPictureBox1"
+        Me.CBPictureBox1.Size = New System.Drawing.Size(149, 10)
+        Me.CBPictureBox1.TabIndex = 14
+        Me.CBPictureBox1.TabStop = False
+        '
+        'DisplayPictureBox
+        '
+        Me.DisplayPictureBox.Location = New System.Drawing.Point(2, 46)
+        Me.DisplayPictureBox.Name = "DisplayPictureBox"
+        Me.DisplayPictureBox.Size = New System.Drawing.Size(447, 392)
+        Me.DisplayPictureBox.TabIndex = 3
+        Me.DisplayPictureBox.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.bunnydex.My.Resources.Resources.images__4_1
+        Me.PictureBox1.Location = New System.Drawing.Point(345, 30)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(141, 10)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.PictureBoxChinchilla)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.CBPictureBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.furLabel)
+        Me.Controls.Add(Me.sizeLabel)
+        Me.Controls.Add(Me.colorLabel)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.DisplayPictureBox)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Bunny Dex"
+        CType(Me.PictureBoxChinchilla, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.CBPictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DisplayPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents DisplayPictureBox As PictureBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents colorLabel As Label
+    Friend WithEvents sizeLabel As Label
+    Friend WithEvents furLabel As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents CBPictureBox1 As PictureBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents PictureBoxChinchilla As PictureBox
+End Class
---
+++ VB/bunnydex/bunnydex/Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        DisplayPictureBox.Image = PictureBox1.Image
+        colorLabel.Text = "Black"
+        sizeLabel.Text = "4.5lbs - 6.5lbs"
+        furLabel.Text = "short, soft, flyback"
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        DisplayPictureBox.Image = CBPictureBox1.Image
+        colorLabel.Text = "Cinnamon brown"
+        sizeLabel.Text = "8.5lbs - 11lbs"
+        furLabel.Text = "short, soft, rarely long"
+
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        DisplayPictureBox.Image = PictureBoxChinchilla.Image
+        colorLabel.Text = "Grey & White"
+        sizeLabel.Text = "9lbs - 12lbs"
+        furLabel.Text = "Short & Thick"
+
+    End Sub
+End Class
+
